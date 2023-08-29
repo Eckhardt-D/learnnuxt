eefcb22eed260aea9b1963731bd3a86b0de8ca47
Eckhardt-D <eckhardt.dreyer@gmail.com>
2023-08-29 07:18:41 +0200

    feat: adds function for checkout

--- functions/nitro.config.ts
+++ functions/nitro.config.ts
@@ -6,9 +6,18 @@ config({
   path: resolve(__dirname, ".env.local"),
 });
 
+config({
+  path: resolve(__dirname, ".env"),
+});
+
 export default defineNitroConfig({
   preset: "firebase",
   runtimeConfig: {
+    stripe: {
+        pk: process.env.APP_ENV === "dev" ? process.env.STRIPE_PK : process.env.STRIPE_PK_LIVE,
+        sk: process.env.APP_ENV === "dev" ? process.env.STRIPE_SK : process.env.STRIPE_SK_LIVE,
+        lifetimeId: process.env.APP_ENV === "dev" ? process.env.STRIPE_LIFETIME_ID : process.env.STRIPE_LIFETIME_ID_LIVE,
+    },
     isDevelopment: process.env.APP_ENV === "dev",
     serviceAccountPath:
       (process.env.APP_ENV === "dev" && process.env.SERVICE_ACCOUNT_PATH) ||
